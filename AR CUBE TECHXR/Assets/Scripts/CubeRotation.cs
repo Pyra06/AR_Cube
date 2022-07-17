790048757d5963dda42b57e19c753c558a8e45cd
Pyra06 <aniruddhasharma867@gmail.com>
2022-07-17 11:08:07 +0530

    Ar Cube

---
+++ AR CUBE TECHXR/Assets/Scripts/CubeRotation.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+//using System.Random;
+
+public class CubeRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void rotateRight()
+    {
+        Vector3 rotation;
+        rotation.x = 0;
+        rotation.y = -90;
+        rotation.z = 0;
+        transform.Rotate(rotation);
+    }
+    public void rotateLeft()
+    {
+        Vector3 rotation;
+        rotation.x = 0;
+        rotation.y = 90;
+        rotation.z = 0;
+        transform.Rotate(rotation);
+    }
+    public void rotateUp()
+    {
+        Vector3 rotation;
+        rotation.x = 0;
+        rotation.y = 0;
+        rotation.z = -90;
+        transform.Rotate(rotation);
+    }
+    public void rotateDown()
+    {
+        Vector3 rotation;
+        rotation.x = 0;
+        rotation.y = 0;
+        rotation.z = 90;
+        transform.Rotate(rotation);
+    }
+    public void rotateRightY()
+    {
+        Vector3 rotation;
+        rotation.x = 0;
+        rotation.y = 0;
+        rotation.z = -90;
+        transform.Rotate(rotation);
+    }
+    public void rotateLeftY()
+    {
+        Vector3 rotation;
+        rotation.x = 0;
+        rotation.y = 0;
+        rotation.z = 90;
+        transform.Rotate(rotation);
+    }
+    public void rotateUpY()
+    {
+        Vector3 rotation;
+        rotation.x = 90;
+        rotation.y = 0;
+        rotation.z = 0;
+        transform.Rotate(rotation);
+    }
+    public void rotateDownY()
+    {
+        Vector3 rotation;
+        rotation.x = -90;
+        rotation.y = 0;
+        rotation.z = 0;
+        transform.Rotate(rotation);
+    }
+
+    public void rotateRightW()
+    {
+        Vector3 rotation;
+        rotation.x = 0;
+        rotation.y = 0;
+        rotation.z = -90;
+        transform.Rotate(rotation);
+    }
+    public void rotateLeftW()
+    {
+        Vector3 rotation;
+        rotation.x = 0;
+        rotation.y = 0;
+        rotation.z = 90;
+        transform.Rotate(rotation);
+    }
+    public void rotateUpW()
+    {
+        Vector3 rotation;
+        rotation.x = -90;
+        rotation.y = 0;
+        rotation.z = 0;
+        transform.Rotate(rotation);
+    }
+    public void rotateDownW()
+    {
+        Vector3 rotation;
+        rotation.x = 90;
+        rotation.y = 0;
+        rotation.z = 0;
+        transform.Rotate(rotation);
+    }
+
+    public void rotateRandom()
+    {
+        int[] sides = new int[] { 90, -90, 0, 180 };
+        //Random rnd = new Random();
+        int index1 = Random.Range(0,4);
+        int index2 = Random.Range(0, 4);
+        int index3 = Random.Range(0, 4);
+
+        Vector3 rotation;
+        rotation.x = sides[index1];
+        rotation.y = sides[index2];
+        rotation.z = sides[index3];
+        transform.Rotate(rotation);
+    }
+}
---
+++ AR CUBE TECHXR/Assets/Scripts/ReadArrow.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadArrow : MonoBehaviour
+{
+    string btnName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                btnName = hit.transform.name;
+
+                switch (btnName)
+                {
+                    case "Left":
+                        Debug.Log("Left");
+                        break;
+                    case "Right":
+                        Debug.Log("Right");
+                        break;
+                    case "Up":
+                        Debug.Log("Up");
+                        break;
+                    case "Down":
+                        Debug.Log("Down");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+    }
+}
